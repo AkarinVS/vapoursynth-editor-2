7f87981a243c3ef7078e72b6c8a04d2dd3006d53
akarin <AkarinVS@users.noreply.github.com>
2021-03-14 15:53:22 -0400

    add source for the vsedit_logger plugin
    Signed-off-by: akarin <AkarinVS@users.noreply.github.com>

---
+++ vsedit/src/vapoursynth/logger.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include <VapourSynth.h>
+
+static void VS_CC loggerLog(const VSMap *in, VSMap *out, void *userData, VSCore *core, const VSAPI *vsapi) {
+	int n = vsapi->propNumElements(in, "name");
+	for (int i = 0; i < n; i++) {
+			const char *msg = vsapi->propGetData(in, "name", i, NULL);
+			vsapi->logMessage(mtWarning, msg);
+	}
+}
+
+VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin) {
+	configFunc("com.vsedit.logger", "vsedit", "VapourSynth Logger", VAPOURSYNTH_API_VERSION, 1, plugin);
+	registerFunc("Logger", "name:data[]:empty", loggerLog, 0, plugin);
+}
